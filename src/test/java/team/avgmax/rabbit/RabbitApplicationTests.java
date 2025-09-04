d7702ef7977b3d4668ba147b998cc0787c5161c1
h001 <57217558+hwan001@users.noreply.github.com>
2025-09-04 16:16:33 +0900

    chore: [RABBIT-2] 백엔드 프로젝트 초기 세팅

---
+++ src/test/java/team/avgmax/rabbit/RabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package team.avgmax.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
